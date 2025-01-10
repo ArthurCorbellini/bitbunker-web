1cd63f14bb521fe0809d7b73470cf12581a42fe2
Arthur Corbellini <arthur.corbellini27@gmail.com>
2025-01-09 23:29:21 -0300

    feat(dashboard): new dashboard route and general adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1>
-        Bitbunker Home Page
-      </h1>
+
     </main>
   )
 }
--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -4,30 +4,44 @@ import Link from "next/link";
 export default function MainHeader() {
   return (
     <header>
-      <div className="bg-red-700">
-        <div className="w-24">
-          <Link href="/">
-            <Image
-              className="dark:invert"
-              src="/globe.svg"
-              alt="logo"
-              width={100}
-              height={100}
-              priority
-            />
-          </Link>
-        </div>
+      <div className="flex items-center space-x-4 p-4">
+        <Link href="/">
+          <Image
+            className="dark:invert"
+            src="/globe.svg"
+            alt="logo"
+            width={50}
+            height={50}
+            priority
+          />
+        </Link>
 
-        <nav>
-          <ul>
+        <nav className="p-4">
+          <ul className="flex space-x-6 items-center">
             <li>
-              <Link href="/tests">Tests</Link>
+              <Link href="/" className="hover:text-blue-400 transition-colors">
+                Home
+              </Link>
             </li>
             <li>
-              <Link href="/orders">Orders</Link>
+              <Link href="/dashboard" className="hover:text-blue-400 transition-colors">
+                Dashboard
+              </Link>
             </li>
             <li>
-              <Link href="/tokens">Tokens</Link>
+              <Link href="/orders" className="hover:text-blue-400 transition-colors">
+                Orders
+              </Link>
+            </li>
+            <li>
+              <Link href="/tokens" className="hover:text-blue-400 transition-colors">
+                Tokens
+              </Link>
+            </li>
+            <li>
+              <Link href="/tests" className="hover:text-blue-400 transition-colors">
+                Tests
+              </Link>
             </li>
           </ul>
         </nav>
