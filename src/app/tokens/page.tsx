3ec47b8a78befaf2ffc3398c32b88d97e79ed8d3
Arthur Corbellini <arthur.corbellini27@gmail.com>
2025-01-11 16:41:43 -0300

    feat(tokens): improvements

--- src/app/tokens/page.tsx
+++ src/app/tokens/page.tsx
@@ -1,5 +1,19 @@
 import { PencilIcon, PlusIcon, TrashIcon } from "@heroicons/react/20/solid";
 
+import RoundedButton from "@/components/buttons/rounded-button";
+import {
+  Body,
+  BodyCell,
+  BodyCellActions,
+  BodyRow,
+  Header,
+  HeaderCell,
+  HeaderCellActions,
+  HeaderRow,
+  Table,
+  TableTitle
+} from "@/components/table/data-table";
+
 interface Token {
   ucid: number;
   name: string;
@@ -13,67 +27,47 @@ export default async function Tokens() {
     throw new Error('API_URL is not defined in environment variables.');
 
   const response = await fetch(url + "/token");
+  if (!response.ok)
+    throw new Error('Failed to fetch Tokens.');
+
   const data: Token[] = await response.json();
 
   return (
-    <div className="p-8">
-      <div className="flex mb-4 items-center w-full space-x-2">
-        <h1 className="text-2xl font-bold">
-          Tokens
-        </h1>
-        <button className="px-3 py-3 bg-gray-700 rounded-full hover:bg-blue-700 transition">
+    <>
+      <TableTitle title="Tokens">
+        <RoundedButton>
           <PlusIcon className="h-5 w-5" />
-        </button>
-      </div>
-      <div className="overflow-x-auto">
-        <table className="w-full">
-          <thead>
-            <tr className="border-b border-t border-gray-800 px-4 py-2 text-left">
-              <th className="px-4 py-2">
-                UCID
-              </th>
-              <th className="px-4 py-2">
-                Nome
-              </th>
-              <th className="px-4 py-2">
-                Symbol
-              </th>
-              <th className="px-4 py-2">
-                Classification
-              </th>
-              <th className="px-4 py-2 w-40 text-center">
-                Actions
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((token) => (
-              <tr key={token.ucid} className="border-b border-gray-800 hover:bg-gray-800 transition-colors">
-                <td className="px-4 py-6">
-                  {token.ucid}
-                </td>
-                <td className="px-4 py-6">
-                  {token.name}
-                </td>
-                <td className="px-4 py-6">
-                  {token.symbol}
-                </td>
-                <td className="px-4 py-6">
-                  {token.classification}
-                </td>
-                <td className="px-4 py-3 space-x-2 text-center">
-                  <button className="px-3 py-3 bg-gray-700 rounded-full hover:bg-blue-700 transition">
-                    <PencilIcon className="h-5 w-5" />
-                  </button>
-                  <button className="px-3 py-3 bg-gray-700 rounded-full hover:bg-blue-700 transition">
-                    <TrashIcon className="h-5 w-5" />
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
+        </RoundedButton>
+      </TableTitle>
+      <Table>
+        <Header>
+          <HeaderRow>
+            <HeaderCell>UCID</HeaderCell>
+            <HeaderCell>Nome</HeaderCell>
+            <HeaderCell>Symbol</HeaderCell>
+            <HeaderCell>Classification</HeaderCell>
+            <HeaderCellActions>Actions</HeaderCellActions>
+          </HeaderRow>
+        </Header>
+        <Body>
+          {data.map((token) => (
+            <BodyRow key={token.ucid}>
+              <BodyCell>{token.ucid}</BodyCell>
+              <BodyCell>{token.name}</BodyCell>
+              <BodyCell>{token.symbol}</BodyCell>
+              <BodyCell>{token.classification}</BodyCell>
+              <BodyCellActions>
+                <RoundedButton>
+                  <PencilIcon className="h-5 w-5" />
+                </RoundedButton>
+                <RoundedButton>
+                  <TrashIcon className="h-5 w-5" />
+                </RoundedButton>
+              </BodyCellActions>
+            </BodyRow>
+          ))}
+        </Body>
+      </Table>
+    </>
   );
 }
