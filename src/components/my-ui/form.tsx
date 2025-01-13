7d3c1c53846c44966895415b6ec109d0b1da14de
Arthur Corbellini <arthur.corbellini27@gmail.com>
2025-01-13 01:17:39 -0300

    feat: new input form components

--- src/components/my-ui/form.tsx
+++ src/components/my-ui/form.tsx
@@ -1,16 +1,50 @@
+import { ChangeEventHandler } from "react";
 
-const InputText = ({ label, addClassName }: {
+const InputText = ({ label, value, onChange, addClassName }: {
   label: string,
+  value: string,
+  onChange: (newValue: string) => void;
   addClassName?: string
 }) => {
-  const className = `p-1 w-full ${addClassName}`;
   return (
-    <div className={className}>
-      <label className="text-sm">{label}</label>
-      <input type="text" className="flex h-9 w-full rounded-md border bg-transparent px-3 py-1 mt-1 text-base border-gray-800" />
+    <div className={`p-1 w-full ${addClassName}`}>
+      <label className="text-sm">
+        {label}
+      </label>
+      <input
+        type="text"
+        value={value}
+        onChange={event => onChange(event.target.value)}
+        className="flex h-9 w-full rounded-md border bg-transparent px-3 py-1 mt-1 text-base border-gray-800" />
     </div>
   );
 }
 
-export { InputText };
+const InputNumber = ({ label, value, onChange, addClassName }: {
+  label: string,
+  value: string,
+  onChange: (newValue: string) => void;
+  addClassName?: string
+}) => {
+  const eventHandler: ChangeEventHandler<HTMLInputElement> = event => {
+    onChange(event.target.value.replace(/\D/g, ''));
+  };
+  return (
+    <div className={`p-1 w-full ${addClassName}`}>
+      <label className="text-sm">
+        {label}
+      </label>
+      <input
+        type="text"
+        value={value}
+        onChange={eventHandler}
+        className="flex h-9 w-full rounded-md border bg-transparent px-3 py-1 mt-1 text-base border-gray-800" />
+    </div>
+  );
+}
+
+export {
+  InputNumber,
+  InputText
+};
 
