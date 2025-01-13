a6e10646398f0b957f2921e85f7f5f0b8e7bde73
Arthur Corbellini <arthur.corbellini27@gmail.com>
2025-01-13 01:17:52 -0300

    feat: create-token form

--- src/app/tokens/create-token.tsx
+++ src/app/tokens/create-token.tsx
@@ -4,13 +4,37 @@ import { PlusIcon } from "@heroicons/react/20/solid";
 import { useState } from "react";
 
 import { PrimaryButton, RoundedButton } from "@/components/my-ui/button";
-import { InputText } from "@/components/my-ui/form";
+import { InputNumber, InputText } from "@/components/my-ui/form";
 import { Modal, ModalBody, ModalFooter, ModalHeader } from "@/components/my-ui/modal";
+import { Token } from "@/interfaces/token.interface";
 
 export default function CreateToken() {
   const [open, setOpen] = useState(false);
   const openDialog = () => setOpen(true);
   const closeDialog = () => setOpen(false);
+
+  const [ucid, setUcid] = useState("");
+  const [name, setName] = useState("");
+  const [symbol, setSymbol] = useState("");
+  const [classification, setClassification] = useState("");
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const token: Token = {
+      ucid: Number(ucid),
+      name,
+      symbol,
+      classification
+    }
+
+    // > falta fazer as validações dos campos
+    // > trocar componente <form> para o <Form> do Next
+    // > consumir api para salvar o token
+    // > criar alertas para mostrar possíveis erros de validação/salvamento
+
+    console.log(token)
+  };
 
   return (
     <>
@@ -19,20 +43,19 @@ export default function CreateToken() {
       </RoundedButton>
 
       <Modal open={open} closeHandler={closeDialog}>
-        <form>
+        <form onSubmit={handleSubmit}>
           <ModalHeader title="Create new Token" description="Form to create token" />
           <ModalBody>
-            <InputText label="UCID" addClassName="md:w-1/2 lg:w-2/12" />
-            <InputText label="Name" addClassName="md:w-1/2 lg:w-6/12" />
-            <InputText label="Symbol" addClassName="md:w-1/2 lg:w-2/12" />
-            <InputText label="Classification" addClassName="md:w-1/2 lg:w-2/12" />
+            <InputNumber label="UCID" value={ucid} onChange={setUcid} addClassName="md:w-1/2 lg:w-2/12" />
+            <InputText label="Name" value={name} onChange={setName} addClassName="md:w-1/2 lg:w-6/12" />
+            <InputText label="Symbol" value={symbol} onChange={setSymbol} addClassName="md:w-1/2 lg:w-2/12" />
+            <InputText label="Classification" value={classification} onChange={setClassification} addClassName="md:w-1/2 lg:w-2/12" />
           </ModalBody>
           <ModalFooter>
-            <PrimaryButton addClassName="ml-auto md:w-1/3 lg:w-4/12">Save</PrimaryButton>
+            <PrimaryButton onClick={handleSubmit} addClassName="ml-auto md:w-1/3 lg:w-4/12">Save</PrimaryButton>
           </ModalFooter>
         </form>
       </Modal>
     </>
-
   );
 }
