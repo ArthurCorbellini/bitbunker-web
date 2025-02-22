a92dd1353557394aa6433c59435f44dbaceb9f70
Arthur Corbellini <arthur.corbellini27@gmail.com>
2025-02-22 17:06:49 -0300

    feat: data-table example components

---
+++ src/app/[locale]/example/data-table/page.tsx
@@ -0,0 +1,39 @@
+"use server"
+
+import { DataTable } from "./_components/data-table"
+import { Payment, columns } from "./columns"
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "s@example.com",
+    },
+    // ...
+  ]
+}
+
+export default async function DemoPage() {
+  const data = await getData()
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
+}
