3e9dc626a5e128e5c154e6b849271f317292ac41
Arthur Corbellini <arthur.corbellini27@gmail.com>
2025-02-24 00:17:34 -0300

    feat: OrderList component improvs

--- src/app/[locale]/orders/_components/OrderList.tsx
+++ src/app/[locale]/orders/_components/OrderList.tsx
@@ -1,7 +1,12 @@
+import { H2, Lead } from "@/components/global/typography";
+import { Button } from "@/components/ui/button";
 import { orderApi } from "@/lib/api/order-api";
+import { Plus } from "lucide-react";
+import { getTranslations } from "next-intl/server";
 import { OrderDataTable } from "./OrderDataTable";
 
 export const OrderList = async () => {
+  const t = await getTranslations("orders");
   const response = await orderApi.fetchOrders();
 
   if (!response.success) {
@@ -9,9 +14,19 @@ export const OrderList = async () => {
     response.error
   }
 
-  if (response.data) {
-    return (
-      <OrderDataTable data={response.data} />
-    );
-  }
+  return (
+    <>
+      <H2>{t("transactions")}</H2>
+      <Lead>{t("transactionsLegend")}</Lead>
+
+      <div className="flex justify-end py-4">
+        <Button>
+          <Plus /> {t("addTransactions")}
+        </Button>
+      </div>
+      {response.data &&
+        <OrderDataTable data={response.data} />
+      }
+    </>
+  );
 }
