3f9d3f2649a883301f4c652d854dd3c40bcd38c4
Arthur Corbellini <arthur.corbellini27@gmail.com>
2025-03-13 23:51:24 -0300

    feat: userNumberFormat hook para o componente NumberFormat

--- src/components/global/my-inputs/hooks/useNumberFormat.ts
+++ src/components/global/my-inputs/hooks/useNumberFormat.ts
@@ -6,21 +6,38 @@ export const useNumberFormat = (
   const locale = useLocale();
   const divisor = Math.pow(10, decimalPlaces);
 
+  const format = (input: string) => {
+    return new Intl.NumberFormat(locale, {
+      style: "decimal",
+      minimumFractionDigits: decimalPlaces,
+      maximumFractionDigits: decimalPlaces,
+    }).format(parseFloat(input) / divisor);
+  }
+
   const formatNumber = (rawValue?: number | string) => {
     let input = (rawValue + "").replace(/\D/g, "");
     if (!input)
       input = "0";
 
-    const formattedValue = new Intl.NumberFormat(locale, {
-      style: "decimal",
-      minimumFractionDigits: decimalPlaces,
-      maximumFractionDigits: decimalPlaces,
-    }).format(parseFloat(input) / divisor);
+    const formattedValue = format(input);
+    const numericValue = parseFloat(input) / divisor;
 
-    return { formattedValue, numericValue: parseFloat(input) / divisor };
+    return { formattedValue, numericValue };
+  }
+
+  const formatStringNumber = (rawValue?: string) => {
+    let input = (rawValue + "").replace(/\D/g, "");
+    if (input === "")
+      return { formattedValue: "", numericValue: "" }
+
+    const formattedValue = format(input);
+    const numericValue = (parseFloat(input) / divisor).toString();
+
+    return { formattedValue, numericValue };
   }
 
   return {
-    formatNumber
+    formatNumber,
+    formatStringNumber
   };
 }
