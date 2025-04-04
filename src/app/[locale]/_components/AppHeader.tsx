39699d0b41d13f940b402b84a95148a56f577c4c
Arthur Corbellini <arthur.corbellini27@gmail.com>
2025-04-03 23:30:25 -0300

    fix: remove old header

--- src/app/[locale]/_components/AppHeader.tsx
+++ src/app/[locale]/_components/AppHeader.tsx
@@ -1,5 +1,5 @@
 import { SidebarTrigger } from "@/components/ui/sidebar"
-import { ModeToggle } from "./main-header/mode-toggle"
+import { ModeToggle } from "./mode-toggle"
 
 export const AppHeader = () => {
   return (
